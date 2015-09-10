d306134f2f79779341b2f05e6f82ff306a0d3c20
AmitaiB <blickstein@gmail.com>
2015-09-10 17:09:59 -0400

    adds THPConstants.h

---
+++ THPhotoBombers/THPConstants.h
@@ -0,0 +1,17 @@
+//
+//  THPConstants.h
+//  THPhotoBombers
+//
+//  Created by Amitai Blickstein on 9/10/15.
+//  Copyright (c) 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface THPConstants : NSObject
+
+extern NSString * const kInstagramClientID;
+extern NSString * const kInstagramClientSecret;
+
+
+@end
