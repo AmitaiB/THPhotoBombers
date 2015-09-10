73dae829bbc83f847ac790e8ecd05391f48c8634
AmitaiB <blickstein@gmail.com>
2015-09-10 10:46:09 -0400

    Initial commit

---
+++ THPhotoBombers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  THPhotoBombers
+//
+//  Created by Amitai Blickstein on 9/10/15.
+//  Copyright (c) 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ THPhotoBombers/THPhotoBombersCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  THPhotoBombersCollectionViewController.h
+//  THPhotoBombers
+//
+//  Created by Amitai Blickstein on 9/10/15.
+//  Copyright (c) 2015 Amitai Blickstein, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THPhotoBombersCollectionViewController : UICollectionViewController
+
+@end
